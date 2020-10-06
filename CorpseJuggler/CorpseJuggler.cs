877e33d3ebe17d46de60c69c99dcabcc94ebb8c4
Daioutzu <54756730+Daioutzu@users.noreply.github.com>
2020-10-06 21:20:36 +0100

    Re-created Corpse Spawner to be standealone
    I've taken the Corpse Spawner out of Advanced Training and reworked it with different code.

---
+++ CorpseJuggler/CorpseJuggler.cs
@@ -0,0 +1,182 @@
+﻿using LLHandlers;
+using LLModdingTools;
+using ModMenuIntergration;
+using UnityEngine;
+
+namespace CorpseJuggler
+{
+    public class CorpseJuggler : MonoBehaviour
+    {
+#pragma warning disable IDE0051 // Remove unused private members
+        public const string modVersion = "1.0.0";
+        public const string repositoryOwner = "Daioutzu";
+        public const string repositoryName = "LLBMM-CorpseJuggler";
+#pragma warning restore IDE0051
+
+        public static ModMenuIntegration MMI = null;
+        private bool initialModIntegration;
+        private Rewired.Controller Controller => Rewired.ReInput.controllers.GetLastActiveController();
+        private Rewired.IGamepadTemplate gamePad;
+        private KeyCode enableCorpseSpawner, spawnCorpseKey, moveBallKey;
+        private static bool gamePadSpawnCorpse, gamePadenableCorpseSpawner, gamePadMoveBall;
+        public int joyStickBallMoveSpeed;
+        public readonly int mouseBallMoveSpeed = 18;
+
+        public static bool CreditsEnabled { get; private set; }
+        private const float DEAD_ZONE = 0.2f;
+        private bool trainingModeStart = false;
+
+        internal delegate void DirectionPressed(float f, bool isGamePad);
+        internal delegate void StandardEvent();
+        internal event DirectionPressed OnCursorMoveVert, OnCursorMoveHori;
+        internal event StandardEvent OnTrainingModeStart, OnTrainingModeEnd, OnSpawnCorpseKeyDown, OnEnableCorpseSpawner, OnMoveBallKeyDown;
+        internal static JOFJHDJHJGI CurrentGameState => DNPFJHMAIBP.HHMOGKIMBNM();
+        internal static GameMode CurrentGameMode => JOMBNFKIHIC.GIGAKBJGFDI.PNJOKAICMNN;
+        internal static bool InGame => World.instance != null && (DNPFJHMAIBP.HHMOGKIMBNM() == JOFJHDJHJGI.CDOFDJMLGLO || DNPFJHMAIBP.HHMOGKIMBNM() == JOFJHDJHJGI.LGILIJKMKOD) && !LLScreen.UIScreen.loadingScreenActive;
+        internal static bool IsOffline => !JOMBNFKIHIC.GDNFJCCCKDM;
+        internal static Spawner CorpseSpawner { get; private set; }
+        internal static CorpseJuggler Instance { get; private set; }
+
+        public static void Initialize()
+        {
+            GameObject gameObject = new GameObject("CorpseJuggler", typeof(CorpseJuggler), typeof(ModMenuIntegration));
+            Instance = gameObject.GetComponent<CorpseJuggler>();
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Start()
+        {
+            Debug.Log("[LLBMM] CorpseJuggler Started");
+            if (MMI == null)
+            {
+                MMI = gameObject.GetComponent<ModMenuIntegration>();
+                Debug.Log("[LLBMM] CorpseJuggler: Added GameObject \"ModMenuIntegration\"");
+            }
+        }
+
+        void ModMenuInit()
+        {
+            if ((MMI != null && initialModIntegration == false) || LLModMenu.ModMenu.Instance.inModSubOptions && LLModMenu.ModMenu.Instance.currentOpenMod == "CorpseJuggler")
+            {
+                PopulateModOptions();
+                if (initialModIntegration == false)
+                {
+                    Debug.Log("[LLBMM] CorpseJuggler: Initial ModMenuIntegration Done");
+                    initialModIntegration = true;
+                };
+            }
+        }
+
+        void PopulateModOptions()
+        {
+            enableCorpseSpawner = MMI.GetKeyCode(MMI.configKeys["(key)enableCorpseJugglerKey"]);
+            spawnCorpseKey = MMI.GetKeyCode(MMI.configKeys["(key)spawnCorpseKey"]);
+            moveBallKey = MMI.GetKeyCode(MMI.configKeys["(key)enableBallPositioningKey"]);
+
+            gamePadenableCorpseSpawner = MMI.GetTrueFalse(MMI.configBools["(bool)enableCorpseSpawnerWithSelect"]);
+            gamePadSpawnCorpse = MMI.GetTrueFalse(MMI.configBools["(bool)SpawnCorpseWithRightStickDown"]);
+            gamePadMoveBall = MMI.GetTrueFalse(MMI.configBools["(bool)enableBallPositioningWithRightStickPressed"]);
+            joyStickBallMoveSpeed = MMI.GetSliderValue("(slider)ballSpeedSlider");
+            CreditsEnabled = MMI.GetTrueFalse(MMI.configBools["(bool)In-GameCredit"]);
+        }
+
+        void GetGamePad()
+        {
+            if (Controller?.ImplementsTemplate<Rewired.IGamepadTemplate>() != null)
+            {
+                gamePad = Controller.GetTemplate<Rewired.IGamepadTemplate>();
+            }
+        }
+
+        void Update()
+        {
+            ModMenuInit();
+            if (GameObject.Find("AdvancedTraining"))
+            {
+                return;
+            }
+            GetGamePad();
+
+            bool selectPressed = (gamePad?.center1.justPressed ?? false) && gamePadenableCorpseSpawner == true;
+            bool rightstickPressed = (gamePad?.rightStick.press.justPressed ?? false) && gamePadMoveBall == true;
+            bool rightStickDown = !Input.anyKey && gamePad?.rightStick.value.y < -0.35 && !(gamePad?.rightStick.valuePrev.y < -0.35) && gamePadSpawnCorpse == true;
+
+            if (CurrentGameMode == GameMode.TRAINING && InGame && IsOffline)
+            {
+                if (trainingModeStart == false)
+                {
+                    trainingModeStart = true;
+                    TrainingModStart();
+                }
+            }
+            else
+            {
+                if (trainingModeStart == true)
+                {
+                    trainingModeStart = false;
+                    TrainingModeEnd();
+                }
+            }
+
+            if (Input.GetKeyDown(enableCorpseSpawner) || selectPressed)
+            {
+                OnEnableCorpseSpawner?.Invoke();
+            }
+            else if (Input.GetKeyDown(spawnCorpseKey) || rightStickDown)
+            {
+                OnSpawnCorpseKeyDown?.Invoke();
+            }
+            else if (Input.GetKeyDown(moveBallKey) || rightstickPressed)
+            {
+                OnMoveBallKeyDown?.Invoke();
+            }
+
+            if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0.025f)
+            {
+                OnCursorMoveHori?.Invoke(Input.GetAxis("Mouse X"), false);
+            }
+            else if (Mathf.Abs(gamePad?.rightStick.value.x ?? 0) > DEAD_ZONE)
+            {
+                OnCursorMoveHori?.Invoke(gamePad.rightStick.value.x, true);
+            }
+
+            if (Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.025f)
+            {
+                OnCursorMoveVert?.Invoke(Input.GetAxis("Mouse Y"), false);
+            }
+            else if (Mathf.Abs(gamePad?.rightStick.value.y ?? 0) > DEAD_ZONE)
+            {
+                OnCursorMoveVert?.Invoke(gamePad.rightStick.value.y, true);
+            }
+        }
+
+        const int GUI_HEIGHT = 1080;
+        const int GUI_WIDTH = 1920;
+        void OnGUI()
+        {
+            if (GameObject.Find("AdvancedTraining"))
+            {
+                GUITools.ScaleGUIToViewPort();
+                GUIStyle label = new GUIStyle(GUI.skin.box)
+                {
+                    fontSize = 14,
+                    alignment = TextAnchor.MiddleLeft,
+                    wordWrap = true,
+                };
+                int height = 60;
+                GUI.Label(new Rect(0, GUI_HEIGHT - height, 390, height), $"Incompatible Mod \"AdvancedTraining - v1.3b\" Detected.\nDisabling \"CorpseJuggler\"", label);
+            }
+        }
+
+        protected virtual void TrainingModStart()
+        {
+            CorpseSpawner = World.instance.gameObject.AddComponent<Spawner>();
+            OnTrainingModeStart?.Invoke();
+        }
+        protected virtual void TrainingModeEnd()
+        {
+            OnTrainingModeEnd?.Invoke();
+            CorpseSpawner = null;
+        }
+    }
+}
---
+++ CorpseJuggler/Spawner.cs
@@ -0,0 +1,301 @@
+﻿using GameplayEntities;
+using LLHandlers;
+using LLModdingTools;
+using UnityEngine;
+
+namespace CorpseJuggler
+{
+    class Spawner : MonoBehaviour
+    {
+
+        static bool UsingController => InputHandler.GetLastActiveController() != Controller.mouseKeyboard;
+        static int CurrentControllerMapID => InputHandler.GetLastActiveController().GetMap().id;
+        static int previousControllerMapID;
+
+
+        private BallEntity ball = null;
+        private PlayerEntity player = null;
+        private OEMHHMCDHGK dummy = null;
+        private HHBCPNCDNDH prevBallSpeed;
+        private IBGCBLLKIHA ballPos;
+        private Vector2 ballMoveDirection;
+        private float ballMoveSpeed;
+        private bool expressCancelled;
+        private bool ballMoveToggle;
+        private bool corpseMode;
+        private bool mouseCursorLocked;
+
+        void TrainingModeStart()
+        {
+            Debug.Log("[LLBMM] \"Spawner\" Invoked TrainingModeStart");
+            dummy = PlayerHandler.instance.CreateDummyPlayerEntity(6);
+            dummy.SetPlayerState(PlayerState.DISABLED);
+            dummy.SetPosition(World.OFF_SCREEN_POSITION);
+            ball = BallHandler.instance.GetBall();
+            player = PlayerHandler.instance.GetPlayerEntity(0);
+        }
+
+        void TrainingModeEnded()
+        {
+            Debug.Log("[LLBMM] \"Spawner\" Invoked TrainingModeEnded");
+            Cursor.lockState = CursorLockMode.None;
+            RestoreControllerExpress(previousControllerMapID);
+            RestoreControllerExpress(CurrentControllerMapID);
+            dummy.player.OKDEILOGKFB();
+            ball = null;
+            Destroy(dummy);
+            Destroy(this);
+        }
+        void OnDestroy()
+        {
+            CorpseJuggler.Instance.OnTrainingModeStart -= TrainingModeStart;
+            CorpseJuggler.Instance.OnTrainingModeEnd -= TrainingModeEnded;
+
+            CorpseJuggler.Instance.OnEnableCorpseSpawner -= EnableCorpseSpawner;
+            CorpseJuggler.Instance.OnSpawnCorpseKeyDown -= SpawnCorpseKeyDown;
+
+            CorpseJuggler.Instance.OnMoveBallKeyDown -= MoveBallKeyDown;
+
+            CorpseJuggler.Instance.OnCursorMoveHori -= MouseMoveHori;
+            CorpseJuggler.Instance.OnCursorMoveVert -= MouseMoveVert;
+        }
+
+        void Awake()
+        {
+            CorpseJuggler.Instance.OnTrainingModeStart += TrainingModeStart;
+            CorpseJuggler.Instance.OnTrainingModeEnd += TrainingModeEnded;
+
+            CorpseJuggler.Instance.OnEnableCorpseSpawner += EnableCorpseSpawner;
+        }
+
+        private void MoveBallKeyDown()
+        {
+            if (ballMoveToggle == false)
+            {
+                CorpseJuggler.Instance.OnCursorMoveHori += MouseMoveHori;
+                CorpseJuggler.Instance.OnCursorMoveVert += MouseMoveVert;
+                ballMoveToggle = true;
+            }
+            else
+            {
+                ballMoveToggle = false;
+                CorpseJuggler.Instance.OnCursorMoveHori -= MouseMoveHori;
+                CorpseJuggler.Instance.OnCursorMoveVert -= MouseMoveVert;
+            }
+        }
+
+        private void EnableCorpseSpawner()
+        {
+            bool inServeState = ball.ballData.ballState == BallState.STANDBY || ball.ballData.ballState == BallState.APPEAR;
+            if (inServeState == false && ball.InHitstunByAPlayer() == false)
+            {
+                if (corpseMode == false)
+                {
+                    CorpseJuggler.Instance.OnMoveBallKeyDown += MoveBallKeyDown;
+                    CorpseJuggler.Instance.OnSpawnCorpseKeyDown += SpawnCorpseKeyDown;
+                    MoveBallKeyDown();
+                    CorpseModeEnabled();
+                }
+                else
+                {
+                    CorpseModeDisabled();
+                    CorpseJuggler.Instance.OnSpawnCorpseKeyDown -= SpawnCorpseKeyDown;
+                    CorpseJuggler.Instance.OnMoveBallKeyDown -= MoveBallKeyDown;
+                }
+            }
+        }
+
+        void StickBallToPlayer()
+        {
+            ball.ballData.lastHitterIndex = 0;
+            ball.SetToTeam(0);
+            ball.SetBallState(BallState.STICK_TO_PLAYER_SERVE);
+        }
+
+        private void MouseMoveVert(float f, bool isGamePad)
+        {
+            ballMoveSpeed = isGamePad ? CorpseJuggler.Instance.joyStickBallMoveSpeed : CorpseJuggler.Instance.mouseBallMoveSpeed;
+            ballMoveDirection.y = f;
+        }
+
+        private void MouseMoveHori(float f, bool isGamePad)
+        {
+            ballMoveSpeed = isGamePad ? CorpseJuggler.Instance.joyStickBallMoveSpeed : CorpseJuggler.Instance.mouseBallMoveSpeed;
+            ballMoveDirection.x = f;
+        }
+
+        private void SpawnCorpseKeyDown()
+        {
+            SpawnCorpse();
+        }
+
+        private void CorpseModeEnabled()
+        {
+            ballPos = ball.GetPosition();
+            ball.ballData.flyDirection = new IBGCBLLKIHA(0, 0);
+            prevBallSpeed = ball.GetFlySpeed(true);
+            ball.hitableData.canHitPlayer = false;
+            ball.SetToTeam((BGHNEHPFHGC)4); //Set to Team NONE
+            AudioHandler.PlaySfx(Sfx.MENU_CONFIRM);
+            corpseMode = true;
+        }
+
+        private void CorpseModeDisabled()
+        {
+            if (expressCancelled)
+            {
+                RestoreControllerExpress(previousControllerMapID);
+            }
+            UnlockCursor();
+            ballMoveToggle = false;
+            expressCancelled = false;
+            ball.SetFlySpeed(prevBallSpeed, true);
+            ball.hitableData.canHitPlayer = true;
+            ball.EndHitstun();
+            ball.SetToTeam(0); //Set ball to Red Team
+            ball.ballData.flyDirection = new IBGCBLLKIHA(0, 0);
+            corpseMode = false;
+            StickBallToPlayer();
+            AudioHandler.PlaySfx(Sfx.MENU_SET);
+        }
+
+        void UpdateExpress()
+        {
+            if (UsingController && expressCancelled == false)
+            {
+                int i = CurrentControllerMapID;
+                previousControllerMapID = i;
+                DisableControllerExpress(previousControllerMapID);
+                expressCancelled = true;
+            }
+
+            if (CurrentControllerMapID != previousControllerMapID && expressCancelled == true)
+            {
+                RestoreControllerExpress(previousControllerMapID);
+                expressCancelled = false;
+            }
+        }
+
+        void DisableControllerExpress(int mapID)
+        {
+            if (UsingController == true)
+            {
+                foreach (var name in Rewired.ReInput.mapping.GetControllerMap(mapID).AllMaps)
+                {
+                    if (!name.actionDescriptiveName.Contains("Express")) continue;
+                    name.enabled = false;
+                }
+#if DEBUG
+                Debug.Log($"[LLBMM] Spawner: Disabled Express for {mapID}"); 
+#endif
+            }
+        }
+
+        void RestoreControllerExpress(int mapID)
+        {
+            foreach (var name in Rewired.ReInput.mapping.GetControllerMap(mapID).AllMaps)
+            {
+                if (!name.actionDescriptiveName.Contains("Express")) continue;
+                name.enabled = true;
+            }
+#if DEBUG
+            Debug.Log($"[LLBMM] Spawner: Re-enabled Express for {mapID}"); 
+#endif
+        }
+
+        private void SpawnCorpse()
+        {
+            if (!ballMoveToggle)
+            {
+                bool isRight = GreaterThanAndEqual(ball.GetPosition().GCPKPHMKLBN, player.GetPosition().GCPKPHMKLBN);
+                ItemHandler.instance.SpawnCorpse(dummy.playerIndex, ball.GetPosition(), isRight ? Side.RIGHT : Side.LEFT);
+            }
+        }
+        void CantHitBall()
+        {
+            if (ball.InHitstunByAPlayer()) return;
+            ball.StartHitstun(new HHBCPNCDNDH(10), HitstunState.NONE);
+            ball.SetPosition(ballPos);
+            if (CorpseJuggler.CurrentGameState == JOFJHDJHJGI.LGILIJKMKOD && ballMoveToggle)
+            {
+                ball.SetColorOutlinesColor(new Color(1f, 1f, 0.4f));
+                return;
+            }
+            ball.SetColorOutlinesColor(new Color(1f, 0.4f, 0.4f));
+            ball.SetScale(new Vector3(2, 2, 2));
+        }
+
+        void MoveBall()
+        {
+            ball.EndHitstun();
+            ball.hitableData.canBeHitByPlayer = false;
+            ball.hitableData.canHitPlayer = false;
+            ball.SetColorOutlinesColor(new Color(0.4f, 1f, 0.4f));
+            ball.ballData.flyDirection = new IBGCBLLKIHA(Floatf(ballMoveDirection.x), Floatf(ballMoveDirection.y));
+            ball.bouncingData.flySpeed = Floatf(ballMoveSpeed * 0.6f);
+            ballMoveDirection = Vector2.zero;
+            ballPos = ball.GetPosition();
+        }
+
+        private void LockCursor()
+        {
+            if (Cursor.lockState == CursorLockMode.None && mouseCursorLocked == false)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                mouseCursorLocked = true;
+            }
+        }
+        private void UnlockCursor()
+        {
+            if (Cursor.lockState >= CursorLockMode.Locked && mouseCursorLocked == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                mouseCursorLocked = false;
+            }
+        }
+
+        void Update()
+        {
+            if (corpseMode == true)
+            {
+                UpdateExpress();
+                if (ballMoveToggle == false || CorpseJuggler.CurrentGameState == JOFJHDJHJGI.LGILIJKMKOD)
+                {
+                    UnlockCursor();
+                    CantHitBall();
+                }
+                else
+                {
+                    LockCursor();
+                    MoveBall();
+                }
+            }
+        }
+        public static bool GreaterThanAndEqual(HHBCPNCDNDH a, HHBCPNCDNDH b)
+        {
+            return HHBCPNCDNDH.OCDKNPDIPOB(a, b);
+        }
+
+        public static HHBCPNCDNDH Floatf(float a)
+        {
+            return HHBCPNCDNDH.NKKIFJJEPOL((decimal)a);
+        }
+
+        const int GUI_HEIGHT = 1080;
+        const int GUI_WIDTH = 1920;
+        void OnGUI()
+        {
+            if (CorpseJuggler.CreditsEnabled && corpseMode)
+            {
+                GUITools.ScaleGUIToViewPort();
+                GUIStyle label = new GUIStyle(GUI.skin.box)
+                {
+                    fontSize = 14,
+                    alignment = TextAnchor.MiddleCenter,
+                };
+                int height = 30;
+                GUI.Label(new Rect(0, GUI_HEIGHT - height, 390, height), $"CorpseJuggler v{CorpseJuggler.modVersion} by {CorpseJuggler.repositoryOwner}", label);
+            }
+        }
+    }
+}
